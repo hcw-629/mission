64ca5616174720cc3d827808257646d702284819
‘hcw-629’ <1395428389@qq.com>
2021-08-13 21:51:54 +0800

    任务进度

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+//创建vm
+new Vue({
+	el:'#app',
+	render: h => h(App),
+	beforeCreate() {
+		Vue.prototype.$bus = this
+	},
+})
